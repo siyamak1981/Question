1cc34d9caa68c7c1fe3b4e45756b2d98f93ae7fa
unknown <siyamak1981@gmail.com>
2021-01-08 00:04:40 +0330

    theared commit

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -9,5 +9,8 @@ $factory->define(Question::class, function (Faker $faker) {
     return [
         'title'=>rtrim($faker->sentence(rand(3,5)),'.'),
         'body'=>$faker->paragraph(rand(3,7),true),
+        'votes'=>rand(-10, 10),
+        'answers'=>rand(0,10),
+        'views'=>rand(0,10),
     ];
 });
