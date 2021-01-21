319e1501c353df44ebcc3e9d60cf220ba4235c50
unknown <siyamak1981@gmail.com>
2021-01-21 14:58:45 +0330

    add favorites migrate

--- app/Question.php
+++ app/Question.php
@@ -56,4 +56,8 @@ class Question extends Model
         $this->best_answer_id = $answer->id;
         $this->save();
     }
+
+    public function favorites(){
+        return $this->belongsToMany(User::class, 'favorites');
+    }
 }
